61157533d623ab42bb6e82621ab179428c326ac0
Kamil <bil@int.pl>
2023-12-28 22:26:36 +0100

    Added option to create a new account.

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,30 +1,36 @@
+import { Alert } from "@mui/material";
 import { useSignIn } from "react-auth-kit";
 import { useFormik } from "formik";
 import axios, { AxiosError } from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Avatar from '@mui/material/Avatar';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { Button, Box, TextField, Grid, Link, Paper, Typography, Container } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
-
+import Avatar from "@mui/material/Avatar";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import {
+  Button,
+  Box,
+  TextField,
+  Grid,
+  Link,
+  Paper,
+  Typography,
+  Container,
+} from "@mui/material";
+import { useTranslation } from "react-i18next";
+import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
 
 const SignIn = () => {
   const [error, setError] = useState("");
   const navigate = useNavigate();
   const { t } = useTranslation();
   const signIn = useSignIn();
-  const apiUrl = process.env.REACT_APP_API_URL;
+  const apiUrl = process.env.REACT_APP_API_URL; // move it to Context API
 
-  const onSubmit = async (values: { username: string; password: string; }) => {
+  const onSubmit = async (values: { username: string; password: string }) => {
     setError("");
 
     try {
-      const response = await axios.post(
-        apiUrl + "/token/",
-        values
-      );
+      const response = await axios.post(apiUrl + "/token/", values);
 
       signIn({
         token: response.data.access,
@@ -33,13 +39,13 @@ const SignIn = () => {
         authState: { username: values.username },
       });
 
-      navigate('/editor');
+      navigate("/editor");
     } catch (err) {
-      if (err && err instanceof AxiosError)
-        setError(err.response?.data.message);
-      else if (err && err instanceof Error) setError(err.message);
-
-      console.log("Error: ", err);
+      if (err && err instanceof AxiosError && err.response?.status == 401)
+        setError(err.response?.status.toString());
+      else {
+        setError("error");
+      }
     }
   };
 
@@ -57,23 +63,26 @@ const SignIn = () => {
         <LanguageSwitcher />
       </Box>
       <Container component="main" maxWidth="xs">
-        <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <Paper elevation={3} style={{ padding: "20px", marginTop: "20px" }}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               <LockOutlinedIcon />
             </Avatar>
           </Box>
           <form onSubmit={formik.handleSubmit}>
-            <Typography variant="h5" align="center">{t('Sign in')}</Typography>
+            <Typography variant="h5" align="center">
+              {t("Sign in")}
+            </Typography>
             <TextField
               variant="outlined"
               margin="normal"
               name="username"
+              label={t("Username")}
               required
               fullWidth
               value={formik.values.username}
               onChange={formik.handleChange}
-              placeholder='Login'
+              placeholder={t("Username")}
               type="text"
             />
             <TextField
@@ -83,13 +92,27 @@ const SignIn = () => {
               fullWidth
               value={formik.values.password}
               onChange={formik.handleChange}
+              label={t("Password")}
               placeholder={t("Password")}
               type="password"
             />
-            <Button type="submit" fullWidth color="primary" variant="contained" style={{ marginTop: '10px', marginBottom: '10px' }}>
+            {error && (
+              <Alert severity="error">
+                {error == "401"
+                  ? t("Incorrect credentials.")
+                  : t("Connection error.")}
+              </Alert>
+            )}
+            <Button
+              type="submit"
+              fullWidth
+              color="primary"
+              variant="contained"
+              style={{ marginTop: "10px", marginBottom: "10px" }}
+            >
               {t("Sign in")}
             </Button>
-          </form >
+          </form>
           <Grid container>
             <Grid item xs>
               <Link href="#" variant="body2">
@@ -104,7 +127,7 @@ const SignIn = () => {
           </Grid>
         </Paper>
       </Container>
-    </Box >
+    </Box>
   );
 };
 
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,114 +1,208 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useTranslation } from 'react-i18next';
-import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
-
+import * as React from "react";
+import { useFormik } from "formik";
+import axios, { AxiosError } from "axios";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { useNavigate } from "react-router-dom";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useTranslation } from "react-i18next";
+import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
+import { useState } from "react";
+import * as Yup from "yup";
+import { useSignIn } from "react-auth-kit";
+import { Alert } from "@mui/material";
 
 const defaultTheme = createTheme();
 
 export default function SignUp() {
-    const { t } = useTranslation();
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get("email"),
-            password: data.get("password"),
-        });
-    };
+  const [error, setError] = useState("");
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const signIn = useSignIn();
+  const apiUrl = process.env.REACT_APP_API_URL; // move it to Context API
 
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <Box display="flex" justifyContent="flex-end" p={2}>
-                <LanguageSwitcher />
-            </Box>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        {t("Sign up")}
-                    </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <TextField
-                                    autoComplete="given-name"
-                                    name="login"
-                                    required
-                                    fullWidth
-                                    id="login"
-                                    label="Login"
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email"
-                                    name="email"
-                                    autoComplete="email"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label={t("Password")}
-                                    type="password"
-                                    id="password"
-                                    autoComplete="new-password"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FormControlLabel
-                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                    label={t("I agree with the regulations.")}
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            {t("Sign up")}
-                        </Button>
-                        <Grid container justifyContent="flex-end">
-                            <Grid item>
-                                <Link href="login" variant="body2">
-                                    {t("Already have an account? Sign in.")}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
-    );
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email is required"),
+      password: Yup.string().required("Password is required"),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password")], "Passwords must match")
+        .required("Password confirmation is required"),
+    }),
+    onSubmit: async (values) => {
+      setError("");
+      console.log(values);
+      try {
+        const response_sign_up = await axios.post(apiUrl + "/sign_up/", values);
+        console.log("first: ", response_sign_up);
+        const response_sign_in = await axios.post(apiUrl + "/token/", values);
+        console.log("second: ", response_sign_in);
+
+        signIn({
+          token: response_sign_in.data.access,
+          expiresIn: 3600,
+          tokenType: "Bearer",
+          authState: { username: values.username },
+        });
+
+        navigate("/editor");
+      } catch (err) {
+        console.log(err);
+        setError("error");
+      }
+    },
+  });
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Box display="flex" justifyContent="flex-end" p={2}>
+        <LanguageSwitcher />
+      </Box>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            {t("Sign up")}
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={formik.handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="username"
+                  required
+                  fullWidth
+                  id="username"
+                  label={t("Username")}
+                  placeholder={t("Username")}
+                  autoFocus
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.username}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  name="email"
+                  autoComplete="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={formik.touched.email && formik.errors.email}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label={t("Password")}
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                  helperText={formik.touched.password && formik.errors.password}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label={t("Confirm password")}
+                  type="password"
+                  id="confirmPassword"
+                  autoComplete="new-password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.confirmPassword}
+                  error={
+                    formik.touched.confirmPassword &&
+                    Boolean(formik.errors.confirmPassword)
+                  }
+                  helperText={
+                    formik.touched.confirmPassword &&
+                    formik.errors.confirmPassword
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label={t("I agree with the regulations.")}
+                />
+              </Grid>
+            </Grid>
+            {error && (
+              <Alert severity="error">
+                {error == "401"
+                  ? t("Incorrect credentials.")
+                  : t("Connection error.")}
+              </Alert>
+            )}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              {t("Sign up")}
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="signin" variant="body2">
+                  {t("Already have an account? Sign in.")}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
