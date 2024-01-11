ac4ea192899dce36876a0762cefbd3f011ae561d
Kamil <bil@int.pl>
2024-01-11 22:09:37 +0100

    Added env variable.

--- src/components/ApiProvider/ApiProvider.tsx
+++ src/components/ApiProvider/ApiProvider.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ApiContext from '../../ApiContext';
+import React from "react";
+import ApiContext from "../../ApiContext";
 
 interface ApiProviderProps {
   children: React.ReactNode;
@@ -9,9 +9,7 @@ const ApiProvider: React.FC<ApiProviderProps> = ({ children }) => {
   const apiUrl = process.env.REACT_APP_API_URL as string;
 
   return (
-    <ApiContext.Provider value={{ apiUrl }}>
-      {children}
-    </ApiContext.Provider>
+    <ApiContext.Provider value={{ apiUrl }}>{children}</ApiContext.Provider>
   );
 };
 
